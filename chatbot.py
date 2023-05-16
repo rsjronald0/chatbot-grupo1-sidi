116632435302e1af06886932c670ac18bba51ed8
rsjronald0 <ronaldorsjunior@fac.pe.senac.br>
2023-05-16 14:27:43 -0300

    adição da biblioteca nltk para utilização de nlp como tecnologia de chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,22 @@
 import requests
 import json
 from flask import Flask, request
+import nltk
+from nltk.chat.util import Chat, reflections
+
+pares = [
+    [
+        r"Oi|Olá|E aí|Ola|E ai",
+        ["Oi! Me chamo SidX.", "Olá! Me chamo SidX."]
+    ],
+    [
+        r"Ajuda|Menu",
+        ["Sou o SidX, um chatbot para auxiliar na contratação da SiDi! Envie a palavra 'iniciar' para começar o processo."]
+    ],
+    # pode incluir outros pares de perguntas e respostas
+]
+
+chatbot = Chat(pares, reflections)
 
 app = Flask(__name__)
 
@@ -90,82 +106,97 @@ def chat():
     msg = json.loads(request.data)
     message = str(msg['mensagem'])
 
-    if started == False and job_ok == False and obrigatory == False and interest == False:
+    if (message.lower() == 'iniciar' or  message.lower() == 'inicia' or message.lower() == 'inicio' or message.lower() == 'inici' or message.lower() == 'start'):
         started = True
+        job_ok = False
+        obrigatory = False
+        interest = False
         return "Olá. Seja bem vindo ao chatbot do SiDi. Me chamo SidX! Qual o código da sua vaga?"
+    if (message.lower() == 'tchau' or  message.lower() == 'xau' or message.lower() == 'chau' or message.lower() == 'adeus' or message.lower() == 'bye'):
+        restart_application()
+        return "Até a próxima!"
+    
+    resposta = chatbot.respond(message)
 
-    if started == True and job_ok == False and obrigatory == False and interest == False:
-        if tries >= 3:
-          restart_application()
-          return "Não foi possível prosseguir. Tente mais tarde"
+    if (resposta):
+        return str(resposta)
+    else:
+        if started == False and job_ok == False and obrigatory == False and interest == False:
+            started = True
+            return "Olá. Seja bem vindo ao chatbot do SiDi. Me chamo SidX! Qual o código da sua vaga?"
 
-        check = check_job_id(message)
-        if check == False:
-          tries += 1
-          return "Código da vaga não existe. Por favor, verifique e informe novamente."
-        else:
-            questions = get_job_questions(message)
-            job_ok = True
-            job_id = message
+        if started == True and job_ok == False and obrigatory == False and interest == False:
+            if tries >= 3:
+                restart_application()
+                return "Não foi possível prosseguir. Tente mais tarde"
 
-            if started == True and job_ok == True:
-                if len(elim_questions) == 0 or len(obri_questions) == 0:
+            check = check_job_id(message)
+            if check == False:
+                tries += 1
+                return "Código da vaga não existe. Por favor, verifique e informe novamente."
+            else:
+                questions = get_job_questions(message)
+                job_ok = True
+                job_id = message
+
+                if started == True and job_ok == True:
+                    if len(elim_questions) == 0 or len(obri_questions) == 0:
+                        for q in questions:
+                            if q['tipo'] == 'eliminatoria':
+                                elim_questions.append(q)
+                            else: 
+                                obri_questions.append(q)
+
+                if elim_count - 1 < len(elim_questions):
+                    elim_count += 1
+                    return "Ótimo! Agora vamos seguir para as perguntas da vaga. Responda 1 para SIM e 0 para NÃO para as seguintes perguntas. " + elim_questions[elim_count - 1]['pergunta']
+
+        if started == True and job_ok == True and obrigatory == False and interest == False:
+
+            answers.append(bool(int(message)))
+            if len(elim_questions) == 0 or len(obri_questions) == 0:
                     for q in questions:
                         if q['tipo'] == 'eliminatoria':
                             elim_questions.append(q)
-                        else: 
+                        else:
                             obri_questions.append(q)
-
-            if elim_count - 1 < len(elim_questions):
+            
+            if elim_count - 1 < len(elim_questions) - 1:
                 elim_count += 1
-                return "Ótimo! Agora vamos seguir para as perguntas da vaga. Responda 1 para SIM e 0 para NÃO para as seguintes perguntas. " + elim_questions[elim_count - 1]['pergunta']
-
-    if started == True and job_ok == True and obrigatory == False and interest == False:
-
-        answers.append(bool(int(message)))
-        if len(elim_questions) == 0 or len(obri_questions) == 0:
-                for q in questions:
-                  if q['tipo'] == 'eliminatoria':
-                    elim_questions.append(q)
-                  else:
-                    obri_questions.append(q)
-        
-        if elim_count - 1 < len(elim_questions) - 1:
-            elim_count += 1
-            return elim_questions[elim_count - 1]['pergunta']
-        else:
-            for a in answers:
-                if a == False:
-                    rejected = True
-            if rejected == True:
-                restart_application()
-                return "Esse conhecimento é obrigatório para essa vaga. Tente outras vagas disponíveis."
+                return elim_questions[elim_count - 1]['pergunta']
             else:
-                obrigatory = True
-                if obri_count - 1 < len(obri_questions) - 1:
-                    obri_count += 1
-                    return "Ok! Agora responda as seguintes perguntas: " + obri_questions[obri_count - 1]['pergunta']
-                
-    if started == True and job_ok == True and obrigatory == True and interest == False:
+                for a in answers:
+                    if a == False:
+                        rejected = True
+                if rejected == True:
+                    restart_application()
+                    return "Esse conhecimento é obrigatório para essa vaga. Tente outras vagas disponíveis."
+                else:
+                    obrigatory = True
+                    if obri_count - 1 < len(obri_questions) - 1:
+                        obri_count += 1
+                        return "Ok! Agora responda as seguintes perguntas: " + obri_questions[obri_count - 1]['pergunta']
+                    
+        if started == True and job_ok == True and obrigatory == True and interest == False:
 
-        answers.append(str(message))
-        if obri_count - 1 < len(obri_questions) - 1:
-            obri_count += 1
-            return obri_questions[obri_count - 1]['pergunta']
-        
-        else:
-            interest = True
-            return "Confirmar sua aplicação à vaga? Digite 1 para SIM e 0 para NÃO."
-        
-    if started == True and job_ok == True and obrigatory == True and interest == True:
+            answers.append(str(message))
+            if obri_count - 1 < len(obri_questions) - 1:
+                obri_count += 1
+                return obri_questions[obri_count - 1]['pergunta']
+            
+            else:
+                interest = True
+                return "Confirmar sua aplicação à vaga? Digite 1 para SIM e 0 para NÃO."
+            
+        if started == True and job_ok == True and obrigatory == True and interest == True:
 
-        if bool(int(message)) == True:
-            job_application(answers, job_id)
-            restart_application()
-            return "Sua candidatura a vaga foi registrada com sucesso. Obrigado por participar. :D"
-        else:
-            restart_application()
-            return "Confirmado sua desistência da vaga."
+            if bool(int(message)) == True:
+                job_application(answers, job_id)
+                restart_application()
+                return "Sua candidatura a vaga foi registrada com sucesso. Obrigado por participar. :D"
+            else:
+                restart_application()
+                return "Confirmado sua desistência da vaga."
 
 
 
